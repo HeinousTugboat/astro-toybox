dd7cd0a21a227d15fb10a036be60cd7b65c94345
Alex Jarman <alex.jarman@joinroot.com>
2024-03-25 15:56:50 -0400

    collections and islands go brrr

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,19 @@
-import rss, { pagesGlobToRssItems, type RSSFeedItem } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
 
 export async function GET(context: APIContext) {
+  const posts = await getCollection("posts");
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site ?? 'some-site',
-    items: await pagesGlobToRssItems(import.meta.glob<RSSFeedItem>('./**/*.md')) as RSSFeedItem[],
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}`
+    })),
     customData: `<language>en-us</language>`,
   });
 }
