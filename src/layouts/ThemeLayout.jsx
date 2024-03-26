873031a817dc45b0096bd4c742f9bbdfa47df72e
Alex Jarman <alex.jarman@joinroot.com>
2024-03-26 11:43:34 -0400

    Changes color to bronze 'cause why not

--- src/layouts/ThemeLayout.jsx
+++ src/layouts/ThemeLayout.jsx
@@ -2,7 +2,8 @@ import { Theme, ThemePanel } from '@radix-ui/themes';
 
 export function ThemeLayout({ children }) {
   return (
-    <Theme accentColor="red" panelBackground="solid" radius="none">
+    <Theme accentColor="brown" panelBackground="solid" radius="none">
+    {/* <Theme accentColor="red" panelBackground="solid" radius="none"> */}
       <ThemePanel />
       { children }
     </Theme>
