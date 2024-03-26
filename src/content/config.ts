9dd50110a9343d27fc14ba509271b1c9bdfbb040
Alex Jarman <alex.jarman@joinroot.com>
2024-03-26 14:21:04 -0400

    Adds a buncha techtonica data to see how cross-referencing works

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,53 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
+
+const techtonicaItems = defineCollection({
+  type: 'data',
+  schema: z.object({
+    name: z.string()
+  })
+});
+
+const techtonicaMachines = defineCollection({
+  type: 'data',
+  schema: z.object({
+    name: z.string()
+  })
+});
+
+const techtonicaRecipes = defineCollection({
+  type: 'data',
+  schema: z.object({
+    name: z.string(),
+    rate: z.number(),
+    machine: reference('techtonica-machines'),
+    inputs: z.array(z.object({
+      item: reference('techtonica-items'),
+      quantity: z.number()
+    })),
+    outputs: z.array(z.object({
+      item: reference('techtonica-items'),
+      quantity: z.number()
+    }))
+  })
+})
 
 // Define a `type` and `schema` for each collection
-const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      // image: z.object({
-      //   url: z.string(),
-      //   alt: z.string()
-      // }),
-      tags: z.array(z.string())
-    })
+const posts = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    tags: z.array(z.string())
+  })
 });
 
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  posts: postsCollection,
+  posts,
+  'techtonica-items': techtonicaItems,
+  'techtonica-machines': techtonicaMachines,
+  'techtonica-recipes': techtonicaRecipes,
 };
